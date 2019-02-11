bf028b8e6d2a3c40e10fd94a3026461123eaad89
Maksym Levytskyi <maksym.levytskyi@mantu.im>
2019-02-11 20:02:10 +0200

    add new module

---
+++ appChevDev/src/main/java/com/dave/and/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.dave.and.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+//    private TextView txt1;
+//    private TextView txt2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
